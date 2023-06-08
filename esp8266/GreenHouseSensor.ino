3312cd6ca23899e85025dc8cdbfd1c662442b6a5
Danilosrr <danilo_srr@hotmail.com>
2023-06-08 03:28:47 -0300

    sensor data sent via http post request

--- esp8266/GreenHouseSensor.ino
+++ esp8266/GreenHouseSensor.ino
@@ -1,21 +1,20 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 #include <DHT.h>
 
 // Definir as constantes de configuração do Wi-Fi
 const char* ssid = "SSID"; 
 const char* password = "PASSWORD";
 
+// Definir as constantes de configuração do servidor
+const char* serverIP = "SERVER_IP";
+const int serverPort = "PORT";
+
 // Definir o pino de dados do sensor DHT
 const int DHTPin = 14;
-
-// Inicializar o objeto DHT
 DHT dht(DHTPin, DHT22);
 
-void setup() {
-  // Inicializar o monitor serial
-  Serial.begin(9600);
-  
-  // Conectar-se à rede Wi-Fi
+void connectWifi() {
   Serial.print("Conectando-se à rede Wi-Fi...");
   WiFi.begin(ssid, password);
   
@@ -23,42 +22,43 @@ void setup() {
     delay(1000);
     Serial.print(".");
   }
-  
   Serial.println("");
   Serial.println("Conexão Wi-Fi estabelecida!");
+}
+
+void sendData() {
+  WiFiClient client;
+  HTTPClient http;
+
+  http.begin(client,"http://" + String(serverIP) + ":" + String(serverPort) + "/sensors");
+  http.addHeader("Content-Type", "application/json");
+
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
   
-  // Inicializar o sensor DHT
+  String dataJson = "{\"temperature\":\"" + String(temperature) + "\",\"humidity\":\"" + String(humidity) + "\"}";
+  int httpResponse = http.POST(dataJson);
+  
+  if(httpResponse==200){
+    Serial.print("Temperatura: " + String(temperature) + "°C\tUmidade: " + String(humidity) + "%");
+  } else {
+    Serial.print("Data not sent");
+  }
+  Serial.println("");
+  http.end();
+}
+
+void setup() {
+  Serial.begin(9600);
+  connectWifi();
   dht.begin();
 }
 
 void loop() {
-  // Verificar a conexão Wi-Fi
   if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("Conexão Wi-Fi perdida. Reconectando...");
-    
-    // Reconectar-se à rede Wi-Fi
-    WiFi.begin(ssid, password);
-    
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(1000);
-      Serial.print(".");
-    }
-    
-    Serial.println("");
-    Serial.println("Conexão Wi-Fi restabelecida!");
+    connectWifi();
   }
   
-  // Medir a temperatura e a umidade
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
-  
-  // Imprimir os valores no monitor serial
-  Serial.print("Temperatura: ");
-  Serial.print(temperature);
-  Serial.print(" °C\tUmidade: ");
-  Serial.print(humidity);
-  Serial.println(" %");
-  
-  // Aguardar 5 segundos
-  delay(5000);
+  sendData();
+  delay(10000);
 }
